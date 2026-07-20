4646694833e2604c6437e7ed6da9e702af03d52d
schvp <goshacheetos@gmail.com>
2026-07-20 15:52:13 +0300

    xlam

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-uvicorn conference_system.app.main:app --reload
--- scripts/run_dev.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import uvicorn
-
-
-if __name__ == "__main__":
-    uvicorn.run("conference_system.app.main:app", host="127.0.0.1", port=8000, reload=True)
